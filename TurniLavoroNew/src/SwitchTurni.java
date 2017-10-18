cb66271319caf1fcfc3dedd75c082d087323b947
Daniele Caputo <caputo.daniele.n1@gmail.com>
2017-10-18 03:12:35 +0200

    SISTEMATA funzione della domenica libera e del venerdi sera

--- TurniLavoroNew/src/SwitchTurni.java
+++ TurniLavoroNew/src/SwitchTurni.java
@@ -10,27 +10,33 @@ public class SwitchTurni {
 			precedenza o meno
 			- 
 		*/
-		Turni sett= new Turni();
+		Turni sett = new Turni();
 		String turniLavoratore [];
-		turniLavoratore= new String[6];
+		turniLavoratore = new String[6];
 		int x = 0;
 		//implementare qualcosa se il Dipendente non ha giorni libero per quella settimana
 		turniLavoratore[giornoLibero] = sett.settimana[giornoLibero][0] + " libero";
 		do {
 			if (x != giornoLibero) {
 				turniLavoratore[x] = sett.settimana[x][0] + " " + sett.settimana[x][MattSera];
-				if (x == 0 & MattSera == 2) {
+				if (x == 0 & MattSera == 2) { //Domenica NACHT
 					dipendente.setOreDomenica(sett.oreNotturne[x][MattSera-1]); //Calcolo oreDomenica
-					turniLavoratore[x] = sett.settimana[5][0] + " " + sett.settimana[5][MattSera+1]; //Venerdì SBAGLIATA POSIZIONE!!!
+				//	turniLavoratore[x] = sett.settimana[5][0] + " " + sett.settimana[5][MattSera+1]; //Venerdì però mi riscrive sopra con i prossimi cicli quando x = 5
 				} 
 				else {
+					if (x == 5 & MattSera == 2 & giornoLibero == 0) {
+						dipendente.setOreNotturne(sett.oreNotturne[x][MattSera]); //Calcolo oreNotturne VENERDI alle 18
+						turniLavoratore[5] = sett.settimana[5][0] + " " + sett.settimana[5][MattSera+1]; //Venerdi alle 18 se domenica NACHT è libero
+					}
+					else {
 					dipendente.setOreNotturne(sett.oreNotturne[x][MattSera-1]); //Calcolo oreNotturne
+					}
 				}
 			}
 			x++;
 		}
 		while (x < 6);
 		return turniLavoratore;
-	}
+}
 }
 
